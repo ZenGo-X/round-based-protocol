a229bc3a2aa35d3dd6a6ebcb4134d400cce67249
Denis <dsurv@yandex.ru>
2021-09-03 18:12:15 +0700

    Move two_party into two_party/insecure

--- round-based/src/delivery/two_party/insecure.rs
+++ round-based/src/delivery/two_party/insecure.rs
@@ -20,29 +20,55 @@ pub const CLIENT_ID: u16 = 1;
 
 use crate::delivery::{DeliverOutgoing, Delivery, Incoming, Outgoing};
 
-/// A link established between two parties that can be used to exchange messages `M`
-pub struct TwoPartyLink<M, R, W> {
+/// A delivery link established between two parties that can be used to exchange messages `M`
+pub struct TwoParty<M, R, W> {
     recv: RecvLink<M, R>,
     send: SendLink<W>,
 }
 
-impl<M, R, W> TwoPartyLink<M, R, W>
+impl<M, R, W> TwoParty<M, R, W>
 where
     M: Serialize + DeserializeOwned + Clone,
     R: AsyncRead + Send + Unpin,
     W: AsyncWrite + Send + Unpin,
 {
-    pub fn new(read_link: R, write_link: W, capacity: usize, counterparty_id: u16) -> Self {
+    /// Constructs a two party link from raw byte channels
+    ///
+    /// * `read_link` is an [`AsyncRead`] that reads bytes sent by counterparty
+    /// * `write_link` is an [`AsyncWrite`] that sends bytes to the counterparty
+    /// * `capacity` is an initial capacity of internal buffers. Buffers grow on sending/receiving
+    ///   larger messages
+    /// * `message_size_limit` limits length of serialized message. Sending/receiving larger messages
+    ///   results into error that closes a channel
+    /// * `side` determines whether this party is a server or a client. \
+    ///   Basically, it affects only counterparty index, ie. `Side::Server` incoming messages will
+    ///   have `sender = 1`, and `Side::Client` incoming messages will have `sender = 0`
+    pub fn new(
+        side: Side,
+        read_link: R,
+        write_link: W,
+        capacity: usize,
+        message_size_limit: usize,
+    ) -> Self {
+        let counterparty_id = match side {
+            Side::Server => CLIENT_ID,
+            Side::Client => SERVER_ID,
+        };
         Self {
-            recv: RecvLink::new(read_link, capacity, counterparty_id),
-            send: SendLink::new(write_link, capacity, counterparty_id),
+            recv: RecvLink::new(read_link, capacity, message_size_limit, counterparty_id),
+            send: SendLink::new(write_link, capacity, message_size_limit, counterparty_id),
         }
     }
 }
 
-pub type TcpTwoPartyLink<M> = TwoPartyLink<M, OwnedReadHalf, OwnedWriteHalf>;
+pub enum Side {
+    Server,
+    Client,
+}
 
-impl<M, R, W> Delivery<M> for TwoPartyLink<M, R, W>
+pub type TcpTwoPartyLink<M> = TwoParty<M, OwnedReadHalf, OwnedWriteHalf>;
+
+impl<M, R, W> Delivery<M> for TwoParty<M, R, W>
 where
     M: Serialize + DeserializeOwned + Clone + Unpin + 'static,
     R: AsyncRead + Send + Unpin + 'static,
@@ -74,7 +100,7 @@ where
             loop {
                 let (conn, remote_addr) = server.accept().await?;
                 let (recv, send) = conn.into_split();
-                yield (TwoPartyLink::new(recv, send, 4096, CLIENT_ID), remote_addr);
+                yield (TwoParty::new(Side::Server, recv, send, 4096, 10_000), remote_addr);
             }
         },
         local_addr,
@@ -87,7 +113,7 @@ where
 {
     let conn = net::TcpStream::connect(connect_to).await?;
     let (recv, send) = conn.into_split();
-    Ok(TwoPartyLink::new(recv, send, 4096, SERVER_ID))
+    Ok(TwoParty::new(Side::Client, recv, send, 4096, 10_000))
 }
 
 pin_project! {
@@ -97,23 +123,32 @@ pin_project! {
     pub struct SendLink<S> {
         #[pin]
         link: S,
-        buffer: Box<[u8]>,
-        buffer_len: usize,
-        bytes_written: usize,
+        buffer: Vec<u8>,
+        buffer_filled: usize,
+        buffer_sent: usize,
+        msg_len_limit: usize,
 
         counterparty_id: u16,
     }
 }
 
 impl<L> SendLink<L> {
-    pub fn new(link: L, capacity: usize, counterparty_id: u16) -> Self {
+    pub fn new(link: L, capacity: usize, msg_len_limit: usize, counterparty_id: u16) -> Self {
         Self {
             link,
-            buffer: vec![0; capacity].into_boxed_slice(),
-            buffer_len: 0,
-            bytes_written: 0,
+            buffer: vec![0; capacity],
+            buffer_filled: 0,
+            buffer_sent: 0,
+            msg_len_limit,
             counterparty_id,
         }
+    }
+}
+
+impl<L> SendLink<L> {
+    fn buffer_capacity(self: Pin<&Self>) -> usize {
+        let me = self.project_ref();
+        me.buffer.len() - me.buffer_filled
     }
 }
 
@@ -122,14 +157,14 @@ where
     M: Serialize + Clone + Unpin,
     S: AsyncWrite,
 {
-    type Prepared = ServerPreparedSend<M>;
+    type Prepared = PreparedSend<M>;
     type Error = io::Error;
 
     fn prepare(self: Pin<&Self>, msg: Outgoing<&M>) -> io::Result<Self::Prepared> {
         if !(msg.recipient.is_none() || msg.recipient == Some(self.counterparty_id)) {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
-                "recipient must be a client",
+                "recipient index mismatched",
             ));
         }
 
@@ -139,13 +174,13 @@ where
             .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?
             .try_into()
             .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-        if usize::from(serialized_size) > me.buffer.len() - 2 {
+        if usize::from(serialized_size) > *me.msg_len_limit {
             return Err(io::Error::new(
                 io::ErrorKind::InvalidInput,
                 "message is too large to fit into the intermediate buffer",
             ));
         }
-        Ok(ServerPreparedSend {
+        Ok(PreparedSend {
             serialized_size,
             msg: msg.msg.clone(),
         })
@@ -158,30 +193,35 @@ where
     ) -> Poll<io::Result<()>> {
         let me = self.as_mut().project();
 
+        // Check if the buffer is able to fit the message
+        if usize::from(msg.serialized_size) + 2 > me.buffer.len() {
+            // if it does not, we need to grow the buffer
+            me.buffer.resize(usize::from(msg.serialized_size) + 2, 0);
+        }
+
         // Check if we have enough capacity in the buffer
-        if usize::from(msg.serialized_size) + 2 > me.buffer.len() - *me.buffer_len {
+        while usize::from(msg.serialized_size) + 2 > self.as_ref().buffer_capacity() {
             // Not enough capacity - need to flush the buffer
-            return match <Self as DeliverOutgoing<M>>::poll_flush(self.as_mut(), cx) {
-                Poll::Ready(Ok(())) => {
-                    // Flush is finished, try to send again
-                    self.poll_start_send(cx, msg)
-                }
-                Poll::Ready(Err(e)) => Poll::Ready(Err(e)),
-                Poll::Pending => Poll::Pending,
+            match <Self as DeliverOutgoing<M>>::poll_flush(self.as_mut(), cx) {
+                Poll::Ready(Ok(())) => (),
+                Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
+                Poll::Pending => return Poll::Pending,
             };
         }
 
+        let me = self.as_mut().project();
+
         // Serialize msg to the buffer
-        me.buffer[*me.buffer_len..*me.buffer_len + 2]
+        me.buffer[*me.buffer_filled..*me.buffer_filled + 2]
             .copy_from_slice(&msg.serialized_size.to_be_bytes());
-        *me.buffer_len += 2;
+        *me.buffer_filled += 2;
 
         bincode::serialize_into(
-            &mut me.buffer[*me.buffer_len..*me.buffer_len + usize::from(msg.serialized_size)],
+            &mut me.buffer[*me.buffer_filled..*me.buffer_filled + usize::from(msg.serialized_size)],
             &msg.msg,
         )
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
-        *me.buffer_len += usize::from(msg.serialized_size);
+        *me.buffer_filled += usize::from(msg.serialized_size);
 
         Poll::Ready(Ok(()))
     }
@@ -189,17 +229,17 @@ where
     fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
         let me = self.as_mut().project();
 
-        if *me.buffer_len == 0 {
+        if *me.buffer_filled == 0 {
             // Have nothing to send
             Poll::Ready(Ok(()))
-        } else if *me.bytes_written < *me.buffer_len {
+        } else if *me.buffer_sent < *me.buffer_filled {
             // We have more data to write
             match me
                 .link
-                .poll_write(cx, &me.buffer[*me.bytes_written..*me.buffer_len])
+                .poll_write(cx, &me.buffer[*me.buffer_sent..*me.buffer_filled])
             {
                 Poll::Ready(Ok(written)) => {
-                    *me.bytes_written += written;
+                    *me.buffer_sent += written;
                     // Need to repeat flush
                     <Self as DeliverOutgoing<M>>::poll_flush(self, cx)
                 }
@@ -209,23 +249,20 @@ where
         } else {
             // We've wrote all the data to the I/O, need to flush it
             ready!(me.link.poll_flush(cx))?;
-            *me.buffer_len = 0;
-            *me.bytes_written = 0;
+            *me.buffer_filled = 0;
+            *me.buffer_sent = 0;
             Poll::Ready(Ok(()))
         }
     }
 
     fn poll_close(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<io::Result<()>> {
-        match <Self as DeliverOutgoing<M>>::poll_flush(self.as_mut(), cx) {
-            Poll::Ready(Ok(())) => {}
-            result => return result,
-        }
+        ready!(<Self as DeliverOutgoing<M>>::poll_flush(self.as_mut(), cx))?;
         let me = self.project();
         me.link.poll_shutdown(cx)
     }
 }
 
-pub struct ServerPreparedSend<M> {
+pub struct PreparedSend<M> {
     msg: M,
     serialized_size: u16,
 }
@@ -238,7 +275,8 @@ pin_project! {
         #[pin]
         link: R,
 
-        frame: Box<[u8]>,
+        frame: Vec<u8>,
+        msg_len_limit: usize,
         bytes_received: usize,
 
         counterparty_id: u16,
@@ -255,13 +293,14 @@ where
     /// Constructs a new receive link
     ///
     /// `link` is a `AsyncRead` that receives bytes from counterparty. `capacity` is a maximum length
-    /// of serialized message (receiving bigger message results into error).  
-    pub fn new(link: L, capacity: usize, counterparty_id: u16) -> Self {
+    /// of serialized message (receiving bigger message results into error).
+    pub fn new(link: L, capacity: usize, msg_len_limit: usize, counterparty_id: u16) -> Self {
         RecvLink {
             link,
-            frame: vec![0; capacity].into_boxed_slice(),
+            frame: vec![0; capacity],
             bytes_received: 0,
             counterparty_id,
+            msg_len_limit,
             _ph: PhantomType::new(),
         }
     }
@@ -277,6 +316,33 @@ fn next_message_available(frame: &[u8], bytes_received: usize) -> bool {
     bytes_received >= usize::from(msg_len) + 2
 }
 
+fn grow_buffer_if_needed(
+    frame: &mut Vec<u8>,
+    bytes_received: usize,
+    msg_len_limit: usize,
+) -> io::Result<()> {
+    if bytes_received < 2 {
+        return Ok(());
+    }
+
+    let msg_len = u16::from_be_bytes(
+        <[u8; 2]>::try_from(&frame[0..2]).expect("we took exactly two first bytes"),
+    );
+
+    if usize::from(msg_len) > msg_len_limit {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "receiving message too long",
+        ));
+    }
+
+    if frame.len() < 2 + usize::from(msg_len) {
+        frame.resize(2 + usize::from(msg_len), 0)
+    }
+
+    Ok(())
+}
+
 impl<M, R> Stream for RecvLink<M, R>
 where
     R: AsyncRead,
@@ -286,64 +352,56 @@ where
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
         let mut me = self.project();
-        loop {
-            // Unless we have message to parse, we need to read more bytes
-            while !next_message_available(&me.frame, *me.bytes_received) {
-                if *me.bytes_received == me.frame.len() {
-                    return Poll::Ready(Some(Err(io::Error::new(
-                        io::ErrorKind::InvalidData,
-                        "received message is too large",
-                    ))));
-                }
 
-                let bytes_received = *me.bytes_received;
-                let mut buf = ReadBuf::new(&mut me.frame[bytes_received..]);
+        // Unless we have message to parse, we need to read more bytes
+        while !next_message_available(&me.frame, *me.bytes_received) {
+            grow_buffer_if_needed(&mut *me.frame, *me.bytes_received, *me.msg_len_limit)?;
 
-                let bytes_received = match me.link.as_mut().poll_read(cx, &mut buf) {
-                    Poll::Ready(Ok(())) => buf.filled().len(),
-                    Poll::Ready(Err(e)) => return Poll::Ready(Some(Err(e))),
-                    Poll::Pending => return Poll::Pending,
-                };
+            let bytes_received = *me.bytes_received;
+            let mut buf = ReadBuf::new(&mut me.frame[bytes_received..]);
 
-                if bytes_received == 0 {
-                    // No data was read => EOF reached
-                    if *me.bytes_received != 0 {
-                        // We have some bytes received but not handled => EOF is unexpected
-                        return Poll::Ready(Some(Err(io::Error::new(
-                            io::ErrorKind::UnexpectedEof,
-                            "got EOF at the middle of the message",
-                        ))));
-                    }
-                    // Otherwise, stream is peacefully terminated
-                    return Poll::Ready(None);
-                }
-                *me.bytes_received += bytes_received;
-            }
-
-            // At this point we know that we received a message which we can parse
-
-            let msg_len = usize::from(u16::from_be_bytes(
-                <[u8; 2]>::try_from(&me.frame[0..2]).expect("we took exactly two first bytes"),
-            ));
-            let msg = &me.frame[2..];
-            let msg = &msg[..msg_len];
-
-            let msg = match bincode::deserialize::<M>(msg) {
-                Ok(m) => m,
-                Err(e) => {
-                    return Poll::Ready(Some(Err(io::Error::new(io::ErrorKind::InvalidData, e))))
-                }
+            let bytes_received = match me.link.as_mut().poll_read(cx, &mut buf) {
+                Poll::Ready(Ok(())) => buf.filled().len(),
+                Poll::Ready(Err(e)) => return Poll::Ready(Some(Err(e))),
+                Poll::Pending => return Poll::Pending,
             };
 
-            // Delete parsed message from the buffer
-            me.frame.copy_within(2 + msg_len.., 0);
-            *me.bytes_received -= 2 + msg_len;
-
-            return Poll::Ready(Some(Ok(Incoming {
-                sender: *me.counterparty_id,
-                msg,
-            })));
+            if bytes_received == 0 {
+                // No data was read => EOF reached
+                if *me.bytes_received != 0 {
+                    // We have some bytes received but not handled => EOF is unexpected
+                    return Poll::Ready(Some(Err(io::Error::new(
+                        io::ErrorKind::UnexpectedEof,
+                        "got EOF at the middle of the message",
+                    ))));
+                }
+                // Otherwise, stream is peacefully terminated
+                return Poll::Ready(None);
+            }
+            *me.bytes_received += bytes_received;
         }
+
+        // At this point we know that we received a message which we need to parse
+
+        let msg_len = usize::from(u16::from_be_bytes(
+            <[u8; 2]>::try_from(&me.frame[0..2]).expect("we took exactly two first bytes"),
+        ));
+        let msg = &me.frame[2..];
+        let msg = &msg[..msg_len];
+
+        let msg = match bincode::deserialize::<M>(msg) {
+            Ok(m) => m,
+            Err(e) => return Poll::Ready(Some(Err(io::Error::new(io::ErrorKind::InvalidData, e)))),
+        };
+
+        // Delete parsed message from the buffer
+        me.frame.copy_within(2 + msg_len.., 0);
+        *me.bytes_received -= 2 + msg_len;
+
+        return Poll::Ready(Some(Ok(Incoming {
+            sender: *me.counterparty_id,
+            msg,
+        })));
     }
 }
 
@@ -359,10 +417,9 @@ mod tests {
     use rand_chacha::ChaCha20Rng;
     use serde::{Deserialize, Serialize};
 
-    use crate::delivery::two_party::TwoPartyLink;
     use crate::delivery::{DeliverOutgoing, DeliverOutgoingExt, Delivery, Incoming, Outgoing};
 
-    use super::{client, server};
+    use super::{client, server, Side, TwoParty};
 
     #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
     pub struct TestMessage(u16);
@@ -462,17 +519,22 @@ mod tests {
     #[tokio::test]
     async fn test_two_party_in_different_settings() {
         // Link size and buffer size are near to msg len. Messages will be transmitted one by one
-        test_duplex_link(115, 115, 100..=100, 10).await;
+        test_duplex_link(115, 115, 115, 100..=100, 10).await;
         // Link size is near to msg len, but buffer size is too small to fit a whole message, so
         // messages will be transmitted part by part
-        test_duplex_link(115, 50, 100..=100, 10).await;
+        test_duplex_link(115, 115, 50, 100..=100, 10).await;
         // Link size is twice larger than message size, but buffer size fits only one and half messages
-        test_duplex_link(250, 150, 100..=100, 10).await;
+        test_duplex_link(115, 250, 150, 100..=100, 10).await;
         // Buffer fits many messages, but link capable to carry only two and half messages
-        test_duplex_link(390, 4000, 100..=100, 10).await;
+        test_duplex_link(115, 390, 4000, 100..=100, 10).await;
+        // Link size is too small to fit one message, so it grows
+        test_duplex_link(115, 50, 115, 100..=100, 10).await;
+        // Just send random chunks of bytes
+        test_duplex_link(1015, 4096, 4096, 100..=1000, 10).await;
     }
 
     async fn test_duplex_link(
+        msg_len_limit: usize,
         link_capacity: usize,
         buffer_size: usize,
         msg_len: RangeInclusive<usize>,
@@ -482,8 +544,20 @@ mod tests {
         let (server_read, server_write) = io::split(server);
         let (client_read, client_write) = io::split(client);
 
-        let server_link = TwoPartyLink::new(server_read, server_write, link_capacity, 1);
-        let client_link = TwoPartyLink::new(client_read, client_write, link_capacity, 0);
+        let server_link = TwoParty::new(
+            Side::Server,
+            server_read,
+            server_write,
+            link_capacity,
+            msg_len_limit,
+        );
+        let client_link = TwoParty::new(
+            Side::Client,
+            client_read,
+            client_write,
+            link_capacity,
+            msg_len_limit,
+        );
 
         test_two_party_link(server_link, client_link, msg_len, msgs_n).await
     }
