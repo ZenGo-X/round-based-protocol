aab76fcdcbe18bf3529e46a8f29ea35ebca3747c
Denis <dsurv@yandex.ru>
2021-09-03 19:03:53 +0700

    Prettify insecure/twoparty

--- round-based/src/delivery/two_party/insecure.rs
+++ round-based/src/delivery/two_party/insecure.rs
@@ -1,5 +1,6 @@
 use std::convert::{TryFrom, TryInto};
 use std::net::SocketAddr;
+use std::ops;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -66,7 +67,7 @@ pub enum Side {
     Client,
 }
 
-pub type TcpTwoPartyLink<M> = TwoParty<M, OwnedReadHalf, OwnedWriteHalf>;
+pub type TwoPartyTcp<M> = TwoParty<M, OwnedReadHalf, OwnedWriteHalf>;
 
 impl<M, R, W> Delivery<M> for TwoParty<M, R, W>
 where
@@ -83,37 +84,166 @@ where
     }
 }
 
-/// Binds a basic TCP server at given address
-pub async fn server<M, A: net::ToSocketAddrs>(
-    bind_at: A,
-) -> io::Result<(
-    impl Stream<Item = io::Result<(TcpTwoPartyLink<M>, SocketAddr)>>,
-    SocketAddr,
-)>
-where
-    M: Serialize + DeserializeOwned + Clone,
-{
-    let server = net::TcpListener::bind(bind_at).await?;
-    let local_addr = server.local_addr()?;
-    Ok((
-        async_stream::try_stream! {
-            loop {
-                let (conn, remote_addr) = server.accept().await?;
-                let (recv, send) = conn.into_split();
-                yield (TwoParty::new(Side::Server, recv, send, 4096, 10_000), remote_addr);
-            }
-        },
-        local_addr,
-    ))
+/// A party of twoparty protocol who runs a TCP server
+///
+/// A wrapper around tokio [TcpListener](net::TcpListener) with overloaded [`accept`](Self::accept)
+/// method that returns [TwoPartyTcp] implementing [Delivery] trait.
+///
+/// If you need twoparty delivery for different transport (eg. QUIC), you'll have to construct
+/// [TwoParty] structure manually.
+pub struct Server<M> {
+    listener: net::TcpListener,
+    buffer_capacity: usize,
+    msg_len_limit: usize,
+    _ph: PhantomType<M>,
 }
 
-pub async fn client<M, A: net::ToSocketAddrs>(connect_to: A) -> io::Result<TcpTwoPartyLink<M>>
+impl<M> Server<M>
 where
     M: Serialize + DeserializeOwned + Clone,
 {
-    let conn = net::TcpStream::connect(connect_to).await?;
-    let (recv, send) = conn.into_split();
-    Ok(TwoParty::new(Side::Client, recv, send, 4096, 10_000))
+    /// Constructs a server from TcpListener
+    pub fn new(listener: net::TcpListener) -> Self {
+        Self {
+            listener,
+            buffer_capacity: 4096,
+            msg_len_limit: 10_000,
+            _ph: PhantomType::new(),
+        }
+    }
+
+    /// Binds a TCP server
+    ///
+    /// Method is similar to a [tokio one](net::TcpListener::bind). To configure socket more precisely,
+    /// please use [Server::new] constructor.
+    pub async fn bind<A: net::ToSocketAddrs>(addrs: A) -> io::Result<Self> {
+        net::TcpListener::bind(addrs).await.map(Self::new)
+    }
+
+    /// Sets internal buffer capacity
+    ///
+    /// Ideally, capacity should be chosen to fit 2 serialized messages, ie. choose it to be
+    /// `2*average_msg_size`. Buffer grows if it's too small to send/receive a single message unless
+    /// it exceeds [message size limit].
+    ///
+    /// [message size limit]: Self::set_message_size_limit
+    ///
+    /// Calling this method affects only subsequently accepted connections.
+    ///
+    /// Default value is 4096 bytes.
+    pub fn set_buffer_capacity(&mut self, capacity: usize) {
+        self.buffer_capacity = capacity
+    }
+
+    /// Limits length of serialized messages being sent or received
+    ///
+    /// Sending / receiving larger messages results into error and causes channel to be closed.
+    ///
+    /// Calling this method affects only subsequently accepted connections.
+    ///
+    /// Default value is 10KB (10 000 bytes).
+    pub fn set_message_size_limit(&mut self, limit: usize) {
+        self.msg_len_limit = limit;
+    }
+
+    /// Accepts a new incoming connection
+    ///
+    /// Returns a [TwoPartyTcp] that implements [Delivery] trait, and address of the client.
+    pub async fn accept(&mut self) -> io::Result<(TwoPartyTcp<M>, SocketAddr)> {
+        let (conn, remote_addr) = self.listener.accept().await?;
+        let (recv, send) = conn.into_split();
+        Ok((
+            TwoParty::new(
+                Side::Server,
+                recv,
+                send,
+                self.buffer_capacity,
+                self.msg_len_limit,
+            ),
+            remote_addr,
+        ))
+    }
+}
+
+impl<M> ops::Deref for Server<M> {
+    type Target = net::TcpListener;
+    fn deref(&self) -> &Self::Target {
+        &self.listener
+    }
+}
+impl<M> ops::DerefMut for Server<M> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.listener
+    }
+}
+
+pub struct ClientBuilder {
+    buffer_capacity: usize,
+    msg_len_limit: usize,
+}
+
+impl ClientBuilder {
+    /// Constructs a client builder
+    pub fn new() -> Self {
+        Self {
+            buffer_capacity: 4096,
+            msg_len_limit: 10_000,
+        }
+    }
+
+    /// Sets internal buffer capacity
+    ///
+    /// Ideally, capacity should be chosen to fit 2 serialized messages, ie. choose it to be
+    /// `2*average_msg_size`. Buffer grows if it's too small to send/receive a single message unless
+    /// it exceeds [message size limit].
+    ///
+    /// [message size limit]: Self::set_message_size_limit
+    ///
+    /// Calling this method affects only subsequently accepted connections.
+    ///
+    /// Default value is 4096 bytes.
+    pub fn set_buffer_capacity(&mut self, capacity: usize) {
+        self.buffer_capacity = capacity
+    }
+
+    /// Limits length of serialized messages being sent or received
+    ///
+    /// Sending / receiving larger messages results into error and causes channel to be closed.
+    ///
+    /// Calling this method affects only subsequently accepted connections.
+    ///
+    /// Default value is 10KB (10 000 bytes).
+    pub fn set_message_size_limit(&mut self, limit: usize) {
+        self.msg_len_limit = limit;
+    }
+
+    /// Opens a TCP connection to a remote host
+    ///
+    /// Similar to [TcpStream::connect](net::TcpStream::connect). To configure connection more precisely,
+    /// please use [connected](Self::connected) constructor
+    pub async fn connect<M, A>(self, addr: A) -> io::Result<TwoPartyTcp<M>>
+    where
+        A: net::ToSocketAddrs,
+        M: Serialize + DeserializeOwned + Clone,
+    {
+        let conn = net::TcpStream::connect(addr).await?;
+        self.connected(conn)
+    }
+
+    /// Constructs TwoPartyTcp from TcpStream
+    pub fn connected<M>(self, tcp_stream: net::TcpStream) -> io::Result<TwoPartyTcp<M>>
+    where
+        M: Serialize + DeserializeOwned + Clone,
+    {
+        let (recv, send) = tcp_stream.into_split();
+        Ok(TwoParty::new(
+            Side::Client,
+            recv,
+            send,
+            self.buffer_capacity,
+            self.msg_len_limit,
+        ))
+    }
 }
 
 pin_project! {
@@ -412,14 +542,14 @@ mod tests {
 
     use tokio::io;
 
-    use futures::{pin_mut, StreamExt, TryStream, TryStreamExt};
+    use futures::{TryStream, TryStreamExt};
     use rand::{random, Rng, RngCore, SeedableRng};
     use rand_chacha::ChaCha20Rng;
     use serde::{Deserialize, Serialize};
 
     use crate::delivery::{DeliverOutgoing, DeliverOutgoingExt, Delivery, Incoming, Outgoing};
 
-    use super::{client, server, Side, TwoParty};
+    use super::{ClientBuilder, Server, Side, TwoParty};
 
     #[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
     pub struct TestMessage(u16);
@@ -428,11 +558,15 @@ mod tests {
     /// exchange messages
     #[tokio::test]
     async fn exchange_server_client_messages() {
-        let (clients, server_addr) = server::<TestMessage, _>("127.0.0.1:0").await.unwrap();
+        let mut server = Server::<TestMessage>::bind("127.0.0.1:0").await.unwrap();
+        let server_addr = server.local_addr().unwrap();
 
         // The client
         let client = tokio::spawn(async move {
-            let link = client::<TestMessage, _>(server_addr).await.unwrap();
+            let link = ClientBuilder::new()
+                .connect::<TestMessage, _>(server_addr)
+                .await
+                .unwrap();
             let (recv, mut send) = link.split();
 
             // Client sends 1+2+3 messages to the server
@@ -472,8 +606,7 @@ mod tests {
 
         // The server
         let server = tokio::spawn(async move {
-            pin_mut!(clients);
-            let (link, _addr) = clients.next().await.unwrap().unwrap();
+            let (link, _addr) = server.accept().await.unwrap();
             let (recv, mut send) = link.split();
 
             // Server sends 1+2+3 messages to the client. Note that messages payload is different from
