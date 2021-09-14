4a6a13b4ef7f45ab9ba59dfbe473538f0ca296a4
Denis Varlakov <dsurv@yandex.ru>
2021-09-14 18:25:52 +0700

    TD: send message from client

--- round-based/src/delivery/trusted_delivery/client/identity_resolver.rs
+++ round-based/src/delivery/trusted_delivery/client/identity_resolver.rs
@@ -2,11 +2,24 @@ use std::convert::TryFrom;
 
 use secp256k1::PublicKey;
 
-pub trait IdentityResolver {
+pub trait IdentityResolver: Clone {
+    type IdentitiesIter: Iterator<Item = PublicKey>;
+
+    /// Number of parties
+    fn number_of_parties(&self) -> u16;
+    /// Returns index of party with specified public key, or `None` if this party's unknown
     fn lookup_party_index(&self, party_identity: &PublicKey) -> Option<u16>;
+    /// Returns public key of i-th party, or `None` if `i >= n`
     fn lookup_party_identity(&self, party_index: u16) -> Option<&PublicKey>;
+
+    /// Iterator over parties identities
+    ///
+    /// First yielded identity correspond to party with index `0`, second identity — to party with
+    /// index `1`, etc.
+    fn identities(&self) -> Self::IdentitiesIter;
 }
 
+#[derive(Clone, Debug)]
 pub struct SortedIdentities(Vec<PublicKey>);
 
 impl From<Vec<PublicKey>> for SortedIdentities {
@@ -23,7 +36,13 @@ impl std::ops::Deref for SortedIdentities {
     }
 }
 
-impl IdentityResolver for Vec<PublicKey> {
+impl IdentityResolver for SortedIdentities {
+    type IdentitiesIter = std::vec::IntoIter<PublicKey>;
+
+    fn number_of_parties(&self) -> u16 {
+        u16::try_from(self.len()).expect("too many parties")
+    }
+
     fn lookup_party_index(&self, party_identity: &PublicKey) -> Option<u16> {
         self.binary_search(party_identity)
             .ok()
@@ -32,5 +51,9 @@ impl IdentityResolver for Vec<PublicKey> {
 
     fn lookup_party_identity(&self, party_index: u16) -> Option<&PublicKey> {
         self.get(usize::from(party_index))
+    }
+
+    fn identities(&self) -> Self::IdentitiesIter {
+        self.0.clone().into_iter()
     }
 }
--- round-based/src/delivery/trusted_delivery/client/insecure.rs
+++ round-based/src/delivery/trusted_delivery/client/insecure.rs
@@ -1,17 +1,22 @@
+use std::convert::TryFrom;
 use std::future::Future;
 use std::pin::Pin;
 use std::task::{Context, Poll};
+
+use thiserror::Error;
 
 use futures::ready;
 use tokio::io::{self, AsyncRead, AsyncWrite};
 use tokio_rustls::{client::TlsStream, Connect as TlsConnect, TlsConnector};
 
-use secp256k1::{Message, PublicKey, SecretKey, SECP256K1};
+use secp256k1::{Message, PublicKey, SecretKey, Signature, SECP256K1};
 use sha2::{Digest, Sha256};
 
-use crate::delivery::utils::tls::ClientTlsConfig;
-
 use crate::delivery::trusted_delivery::message::{HelloMsg, HELLO_MSG_LEN};
+use crate::delivery::utils::tls::ClientTlsConfig;
+use crate::Outgoing;
+
+use super::identity_resolver::IdentityResolver;
 
 type TlsHandshake<IO> =
     crate::delivery::trusted_delivery::tls_handshake::TlsHandshake<TlsConnect<IO>, TlsStream<IO>>;
@@ -19,64 +24,89 @@ type TlsHandshake<IO> =
 pub struct Connector {
     tls_connector: TlsConnector,
     identity_key: SecretKey,
+    identity: PublicKey,
 }
 
-pub struct Connect<IO> {
+pub struct Connect<P, IO> {
     handshake: TlsHandshake<IO>,
     identity_key: SecretKey,
+    parties: P,
     hello_msg: [u8; HELLO_MSG_LEN],
     sent_bytes: usize,
 }
 
+#[derive(Debug, Error)]
+#[error("{0}")]
+pub struct MalformedListOfParties(#[from] Reason);
+
+#[derive(Debug, Error)]
+pub enum Reason {
+    #[error("list of parties doesn't include identity of local party")]
+    DoesntIncludeIdentityOfLocalParty,
+    #[error("list of parties consist of {n} identity, at least 2 are required")]
+    TooFewParties { n: u16 },
+}
+
 impl Connector {
     pub fn new(tls_config: &ClientTlsConfig, identity_key: SecretKey) -> Self {
-        Self {
-            tls_connector: TlsConnector::from(tls_config.to_rustls_config()),
+        Self::with_connector(
+            TlsConnector::from(tls_config.to_rustls_config()),
             identity_key,
-        }
+        )
     }
 
     pub fn with_connector(tls_connector: TlsConnector, identity_key: SecretKey) -> Self {
         Self {
             tls_connector,
+            identity: PublicKey::from_secret_key(&SECP256K1, &identity_key),
             identity_key,
         }
     }
 
-    pub fn connect<IO>(
+    pub fn connect<P, IO>(
         &self,
+        room_id: [u8; 32],
+        parties: P,
         domain: webpki::DNSNameRef,
         stream: IO,
-        room_id: [u8; 32],
-    ) -> Connect<IO>
+    ) -> Result<Connect<P, IO>, MalformedListOfParties>
     where
         IO: AsyncRead + AsyncWrite + Unpin,
+        P: IdentityResolver,
     {
+        if parties.lookup_party_index(&self.identity).is_none() {
+            return Err(Reason::DoesntIncludeIdentityOfLocalParty.into());
+        }
+        if parties.number_of_parties() < 2 {
+            return Err(Reason::TooFewParties { n: 1 }.into());
+        }
         let hashed_msg = Sha256::digest(&room_id);
         let hashed_msg = Message::from_slice(&hashed_msg)
             .expect("message has appropriate length, from_slice must never fail");
 
         let signature = SECP256K1.sign(&hashed_msg, &self.identity_key);
         let hello_msg = HelloMsg {
-            public_key: PublicKey::from_secret_key(&SECP256K1, &self.identity_key),
+            public_key: self.identity,
             room_id,
             signature,
         };
 
-        Connect {
+        Ok(Connect {
             handshake: TlsHandshake::InProgress(self.tls_connector.connect(domain, stream)),
             identity_key: self.identity_key,
+            parties,
             hello_msg: hello_msg.to_bytes(),
             sent_bytes: 0,
-        }
+        })
     }
 }
 
-impl<IO> Future for Connect<IO>
+impl<P, IO> Future for Connect<P, IO>
 where
     IO: AsyncRead + AsyncWrite + Unpin,
+    P: IdentityResolver + Unpin,
 {
-    type Output = io::Result<ConnectedClient<IO>>;
+    type Output = io::Result<ConnectedClient<P, IO>>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         let Self {
@@ -103,11 +133,128 @@ where
                 .ok()
                 .expect("guaranteed to be completed"),
             identity_key: self.identity_key,
+            parties: self.parties.clone(),
         }))
     }
 }
 
-pub struct ConnectedClient<IO> {
+pub struct ConnectedClient<P, IO> {
     stream: TlsStream<IO>,
     identity_key: SecretKey,
+    parties: P,
+}
+
+pub struct OutgoingDelivery<P, IO> {
+    channel: io::WriteHalf<TlsStream<IO>>,
+    identity_key: SecretKey,
+    parties: P,
+}
+
+pub struct PreparedOutgoing<'b> {
+    header: [u8; 36],
+    header_len: HeaderLength,
+    msg: &'b [u8],
+    signature: [u8; 64],
+    sent_bytes: usize,
+}
+
+#[derive(Clone, Copy, Debug)]
+#[repr(usize)]
+enum HeaderLength {
+    Broadcast = 3,
+    P2P = 36,
+}
+
+#[derive(Debug, Error)]
+pub enum MalformedMessage {
+    #[error("destination party is unknown")]
+    UnknownParty(u16),
+    #[error("message too long: len={len}, limit={}", u16::MAX)]
+    MessageTooLong { len: usize },
+}
+
+impl<P, IO> OutgoingDelivery<P, IO>
+where
+    P: IdentityResolver + Unpin,
+    IO: AsyncRead + AsyncWrite + Unpin,
+{
+    pub fn prepare<'b>(
+        &self,
+        msg: Outgoing<&'b [u8]>,
+    ) -> Result<PreparedOutgoing<'b>, MalformedMessage> {
+        let msg_len = u16::try_from(msg.msg.len())
+            .or(Err(MalformedMessage::MessageTooLong { len: msg.msg.len() }))?;
+        let recipient = msg
+            .recipient
+            .map(|idx| {
+                self.parties
+                    .lookup_party_identity(idx)
+                    .ok_or(MalformedMessage::UnknownParty(idx))
+            })
+            .transpose()?;
+
+        let mut header = [0u8; 36];
+        let header_len;
+        match recipient {
+            Some(recipient) => {
+                header[0] = 1;
+                header[1..34].copy_from_slice(&recipient.serialize());
+                header[34..36].copy_from_slice(&msg_len.to_be_bytes());
+                header_len = HeaderLength::P2P;
+            }
+            None => {
+                header[0] = 0;
+                header[1..3].copy_from_slice(&msg_len.to_be_bytes());
+                header_len = HeaderLength::Broadcast;
+            }
+        };
+
+        let hashed_msg = Sha256::new()
+            .chain(recipient.map(PublicKey::serialize).unwrap_or([0u8; 33]))
+            .chain(msg.msg)
+            .finalize();
+        let hashed_msg =
+            Message::from_slice(hashed_msg.as_slice()).expect("sha256 output can be signed");
+
+        let signature = SECP256K1
+            .sign(&hashed_msg, &self.identity_key)
+            .serialize_compact();
+
+        Ok(PreparedOutgoing {
+            header,
+            header_len,
+            msg: msg.msg,
+            signature,
+            sent_bytes: 0,
+        })
+    }
+
+    pub fn poll_send(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context,
+        msg: &mut PreparedOutgoing,
+    ) -> Poll<io::Result<()>> {
+        let header_len = msg.header_len as usize;
+        while msg.sent_bytes < header_len {
+            let header = &msg.header[msg.sent_bytes..header_len];
+            let sent_bytes = ready!(Pin::new(&mut self.channel).poll_write(cx, header))?;
+            msg.sent_bytes += sent_bytes;
+        }
+
+        while msg.sent_bytes < header_len + msg.msg.len() {
+            let offset = msg.sent_bytes - header_len;
+            let sent_bytes =
+                ready!(Pin::new(&mut self.channel).poll_write(cx, &msg.msg[offset..]))?;
+            msg.sent_bytes += sent_bytes;
+        }
+
+        while msg.sent_bytes < header_len + msg.msg.len() + 64 {
+            let offset = msg.sent_bytes - (header_len + msg.msg.len());
+            let sent_bytes =
+                ready!(Pin::new(&mut self.channel).poll_write(cx, &msg.signature[offset..]))?;
+            msg.sent_bytes += sent_bytes;
+        }
+
+        Poll::Ready(Ok(()))
+    }
 }
